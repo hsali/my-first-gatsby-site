7bf50d7d44e65d7b03bde12b412715f767f62f1c
Hafiz Shehbaz Ali <shehbaz.bsee1395@gmail.com>
2022-05-09 00:37:57 +0400

    some pages added.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,14 @@
-import * as React from "react"
+import * as React from 'react'
+import { Link } from 'gatsby'
 
-
-// markup
 const AboutPage = () => {
     return (
-        <h1> About Page</h1>
+        <main>
+            <title>About Me</title>
+            <h1>About Me</h1>
+            <Link to="/">Back to Home</Link>
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </main>
     )
 }
 
